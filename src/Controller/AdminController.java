adad9f5f050023e574ce2f98d123c8ed0405bb91
AmirMJvd <amir.m.jvd.1.1@gmail.com>
2025-01-23 04:06:20 +0330

    اضافه کردن بخش ویرایش محصول

--- src/Controller/AdminController.java
+++ src/Controller/AdminController.java
@@ -7,10 +7,13 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import main.Main;
+import main.MyListener;
+import model.Book;
+import model.BookLists;
 import model.Cart;
 import model.Report;
 
@@ -64,6 +67,46 @@ public class AdminController implements Initializable {
 
     @FXML
     private GridPane grid;
+
+    //-------
+    @FXML
+    private Label CategoryLab;
+
+    @FXML
+    private ImageView bookImg;
+
+    @FXML
+    private Label bookNameLable;
+
+    @FXML
+    private VBox chosenBookCard;
+
+    @FXML
+    private Label countLab;
+
+    @FXML
+    private Label countLabel;
+
+    @FXML
+    private Label nasherLab;
+
+    @FXML
+    private Label translatorLab;
+
+    @FXML
+    private Label writerLab;
+
+    @FXML
+    private TextField PriceTxt;
+
+    @FXML
+    private GridPane grid1;
+
+
+
+
+
+
 
     @FXML
     void CameBack(ActionEvent event) throws IOException {
@@ -171,6 +214,20 @@ public class AdminController implements Initializable {
 
     public void initialize(URL location, ResourceBundle resources) {
         reports.addAll(getReportData());
+        BookLists bookLists = getData();
+        allBooks.addAll(bookLists.getAllBooks());
+        scientificBooks.addAll(bookLists.getScientificBooks());
+        politicalBooks.addAll(bookLists.getPoliticalBooks());
+        psychologyBooks.addAll(bookLists.getPsychologyBooks());
+        if (allBooks.size() > 0) {
+            setChosenFruit(allBooks.get(0));
+            myListener = new MyListener() {
+                @Override
+                public void onClickListener(Book fruit) {
+                    setChosenFruit(fruit);
+                }
+            };
+        }
         int row = 1;
         int column = 0;
         try {
@@ -186,7 +243,7 @@ public class AdminController implements Initializable {
                     column = 0;
                     row++;
                 }
-                grid.add(HBox,column++,row);
+                grid.add(HBox, column++, row);
                 grid.setMinWidth(Region.USE_COMPUTED_SIZE);
                 grid.setPrefWidth(Region.USE_COMPUTED_SIZE);
                 grid.setMaxWidth(Region.USE_PREF_SIZE);
@@ -197,10 +254,201 @@ public class AdminController implements Initializable {
 
                 GridPane.setMargin(HBox, new Insets(10));
             }
+             row = 1;
+             column = 0;
+                for (int i = 0; i < allBooks.size(); i++) {
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    fxmlLoader.setLocation(getClass().getResource("/views/item.fxml"));
+                    AnchorPane anchorPane = fxmlLoader.load();
+                    ItemController itemController = fxmlLoader.getController();
+                    itemController.setData(allBooks.get(i), myListener);
+
+                    if (column == 3) {
+                        column = 0;
+                        row++;
+                    }
+
+                    grid1.add(anchorPane, column++, row); //(child,column,row)
+                    //set grid width
+                    grid1.setMinWidth(Region.USE_COMPUTED_SIZE);
+                    grid1.setPrefWidth(Region.USE_COMPUTED_SIZE);
+                    grid1.setMaxWidth(Region.USE_PREF_SIZE);
+
+                    //set grid height
+                    grid1.setMinHeight(Region.USE_COMPUTED_SIZE);
+                    grid1.setPrefHeight(Region.USE_COMPUTED_SIZE);
+                    grid1.setMaxHeight(Region.USE_PREF_SIZE);
+
+
+                    GridPane.setMargin(anchorPane, new Insets(10));
+                }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private List<Book> allBooks = new ArrayList<>();
+    List<Book> scientificBooks = new ArrayList<>();
+    List<Book> politicalBooks = new ArrayList<>();
+    List<Book> psychologyBooks = new ArrayList<>();
+    private Image image;
+    private MyListener myListener;
+    private Book selectedBook;
+    private BookLists getData() {
+        List<Book> allBooks = new ArrayList<>();
+        List<Book> scientificBooks = new ArrayList<>();
+        List<Book> politicalBooks = new ArrayList<>();
+        List<Book> psychologyBooks = new ArrayList<>();
+        Book book;
+        try {
+            File BookInfo = new File("BookInf.txt");
+            Scanner myReader = new Scanner(BookInfo);
+
+            while (myReader.hasNextLine()) {
+                book = new Book();
+                book.setName(myReader.nextLine());
+                book.setPrice(myReader.nextLine());
+                book.setImgSrc(myReader.nextLine());
+                book.setColor(myReader.nextLine());
+                book.setWriter(myReader.nextLine());
+                book.setTranslator(myReader.nextLine());
+                book.setNasher(myReader.nextLine());
+                book.setCount(myReader.nextLine());
+                String category = myReader.nextLine();
+                book.setCategory(category);
+                switch (category) {
+                    case "علمی":
+                        scientificBooks.add(book);
+                        break;
+                    case "سیاسی":
+                        politicalBooks.add(book);
+                        break;
+                    case "روانشناسی":
+                        psychologyBooks.add(book);
+                        break;
+                    default:
+                        break;
+                }
+                allBooks.add(book);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new BookLists(allBooks, scientificBooks, politicalBooks, psychologyBooks);
+    }
+    private void setChosenFruit(Book book) {
+        selectedBook = book;
+        bookNameLable.setText(book.getName());
+        PriceTxt.setText(Main.CURRENCY + book.getPrice());
+        writerLab.setText(book.getWriter());
+        translatorLab.setText(book.getTranslator());
+        nasherLab.setText(book.getNasher());
+        countLab.setText(book.getCount());
+        countLabel.setText(book.getCount());
+        CategoryLab.setText(book.getCategory());
+        image = new Image(getClass().getResourceAsStream(book.getImgSrc()));
+        bookImg.setImage(image);
+        chosenBookCard.setStyle("-fx-background-color: #" + book.getColor() + ";\n" + "    -fx-background-radius: 30;");
+
+    }
+
+            @FXML
+    void decreaseBtn(ActionEvent event) {
+                try {
+                    // دریافت مقدار فعلی از countLabel
+                    int currentCount = Integer.parseInt(countLabel.getText());
+                    // کاهش مقدار (اگر از صفر بزرگ‌تر است)
+                    if (currentCount > 0) {
+                        currentCount--;
+                    }
+                    // به‌روزرسانی countLabel
+                    countLabel.setText(String.valueOf(currentCount));
+                } catch (NumberFormatException e) {
+                    Report.setText("خطا: مقدار تعداد نامعتبر است!");
+                    e.printStackTrace();
+                }
+    }
+
+    @FXML
+    void increaseBtn(ActionEvent event) {
+        try {
+            // دریافت مقدار فعلی از countLabel
+            int currentCount = Integer.parseInt(countLabel.getText());
+            // افزایش مقدار
+            currentCount++;
+            // به‌روزرسانی countLabel
+            countLabel.setText(String.valueOf(currentCount));
+        } catch (NumberFormatException e) {
+            Report.setText("خطا: مقدار تعداد نامعتبر است!");
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void AddChange(ActionEvent event) {
+        String bookNameToUpdate = bookNameLable.getText();
+        String newPrice = PriceTxt.getText();
+        String newCount = countLabel.getText();
+
+        if (bookNameToUpdate.isEmpty() || newPrice.isEmpty() || newCount.isEmpty()) {
+            Report.setText("لطفاً تمام اطلاعات را وارد کنید!");
+            return;
+        }
+
+        try {
+            File inputFile = new File("BookInf.txt");
+            File tempFile = new File("TempBookInf.txt");
+
+            Scanner scanner = new Scanner(inputFile);
+            FileWriter writer = new FileWriter(tempFile);
+
+            boolean found = false;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                if (line.equals(bookNameToUpdate)) {
+                    // اگر نام کتاب پیدا شد
+                    found = true;
+                    writer.write(line + "\n"); // نام کتاب
+                    writer.write(newPrice + "\n"); // قیمت جدید
+                    scanner.nextLine(); // رد کردن قیمت قبلی
+                    writer.write(scanner.nextLine() + "\n"); // آدرس تصویر
+                    writer.write(scanner.nextLine() + "\n"); // رنگ
+                    writer.write(scanner.nextLine() + "\n"); // نویسنده
+                    writer.write(scanner.nextLine() + "\n"); // مترجم
+                    writer.write(scanner.nextLine() + "\n"); // ناشر
+                    writer.write(newCount + "\n"); // تعداد جدید
+                    scanner.nextLine(); // رد کردن تعداد قبلی
+                    writer.write(scanner.nextLine() + "\n"); // دسته‌بندی
+                } else {
+                    writer.write(line + "\n");
+                }
+            }
+
+            scanner.close();
+            writer.close();
+
+            if (!found) {
+                Report.setText("کتاب موردنظر پیدا نشد!");
+                return;
+            }
+
+            // جایگزینی فایل اصلی با فایل موقت
+            if (inputFile.delete()) {
+                tempFile.renameTo(inputFile);
+                Report.setText("تغییرات با موفقیت ذخیره شد!");
+            } else {
+                Report.setText("خطا در ذخیره تغییرات!");
+            }
+        } catch (IOException e) {
+            Report.setText("خطا در دسترسی به فایل!");
+            e.printStackTrace();
+        }
+    }
+
 }
 
 
