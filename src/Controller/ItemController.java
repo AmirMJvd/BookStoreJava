d3c02e6ad46ef49ccf4a9dbb303104608166c1fb
AmirMJvd <amir.m.jvd.1.1@gmail.com>
2025-02-09 22:03:51 +0330

    اضافه کردن بخش مشخصات کتاب البته هنوز دکمه افزودن به سبد خرید و افزودن به لیست مورد علاقه و دانلود خلاصه و اطلاعاتز کتاب هم ناقص هست

--- src/Controller/ItemController.java
+++ src/Controller/ItemController.java
@@ -1,13 +1,18 @@
 package Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import main.Main;
 import main.MyListener;
 import model.Book;
+
+import java.io.IOException;
 
 public class ItemController {
     @FXML
@@ -34,5 +39,21 @@ public class ItemController {
         priceLable.setText(Main.CURRENCY + book.getPrice());
         Image image = new Image(getClass().getResourceAsStream(book.getImgSrc()));
         img.setImage(image);
+    }
+    @FXML
+    void openInfo(MouseEvent event) {
+        try {
+            // مقدار نام کتاب را به InfoController ارسال کنید
+            infoController.bookName = nameLabel.getText();
+
+            // بارگذاری FXML جدید
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../views/info.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(fxmlLoader.load()));
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
